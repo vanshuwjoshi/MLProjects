1fc1ac62c7971605551a58f268c9688229b6e4ef
Vansh <vanshjoshi612@gmail.com>
2023-09-16 15:45:36 -0400

    data transformation is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass  # used to create class variables
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 # for data ingestion we need input like the location for saving the training, test and complete data
 # this class will help with that
@@ -68,3 +70,6 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
