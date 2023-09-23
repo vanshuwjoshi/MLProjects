c9bb53f0f777d80832e6c1ea58e3f615149e4364
Vansh <vanshjoshi612@gmail.com>
2023-09-22 23:01:01 -0400

    Created app.py and Prediction Pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,61 @@
+# will create functions that will help interact with the web application and grenerate the predictions
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+
+# will load the model.pkl and preprocessor.pkl and transform the captured form data using the preprocessor
+# and predict the math score using the model stored in model.pkl
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            model_path = "artifact\model.pkl"
+            preprocessor_path = "artifact\preprocessor.pkl"
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+            prediction = model.predict(data_scaled)
+            return prediction
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+# this class is responsible for the mapping all the inputs we are giving to the html to the backend
+class CustomData:
+    def __init__(
+        self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education: str,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int,
+    ):
+        self.gender = gender
+        self.race_ethnicity = race_ethnicity
+        self.parental_level_of_education = parental_level_of_education
+        self.lunch = lunch
+        self.test_preparation_course = test_preparation_course
+        self.reading_score = reading_score
+        self.writing_score = writing_score
+
+    # transform a dictionary (data captured from the form) into a dataframe and return it
+    def get_data_as_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race/ethnicity": [self.race_ethnicity],
+                "parental level of education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test preparation course": [self.test_preparation_course],
+                "reading score": [self.reading_score],
+                "writing score": [self.writing_score],
+            }
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -6,35 +6,47 @@ from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True) # make the directory and does not do anything if the directory already exists 
+        os.makedirs(
+            dir_path, exist_ok=True
+        )  # make the directory and does not do anything if the directory already exists
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj) # save the obj
+            dill.dump(obj, file_obj)  # save the obj
     except Exception as e:
         raise CustomException(e, sys)
 
+
 # this model will fit all the models in the dictionary models to out train and test data and return the R2 scores of each model
-def evaluate_models(X_train,y_train,X_test,y_test,models,params):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
             param = params[list(models.keys())[i]]
-            
-            #Grid search cross validation
-            gs = GridSearchCV(model,param,cv=3)
-            gs.fit(X_train,y_train)
-            
+
+            # Grid search cross validation
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
             # fit the model with the best parameters
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-            
+            model.fit(X_train, y_train)
+
             # model.fit(X_train,y_train)
             y_test_pred = model.predict(X_test)
-            test_model_score = r2_score(y_test,y_test_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
             report[list(models.keys())[i]] = test_model_score
         return report
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+
+# this function will load the file from the specific given path (mainly used to load model.pkl and preprocessor.pkl)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
