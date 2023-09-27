be201cda6be0cf646724402c76ab6d25befabf86
Vansh <vanshjoshi612@gmail.com>
2023-09-27 01:04:21 -0400

    updates

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -3,17 +3,17 @@ import os
 import sys
 from dataclasses import dataclass
 
-# from catboost import CatBoostRegressor
+from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
-    # GradientBoostRegressor,
+    GradientBoostRegressor,
     RandomForestRegressor,
 )
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 
-# from xgboost import XGBRegressor
+from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
@@ -45,11 +45,11 @@ class ModelTrainer:
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
-                # "Gradient Boosting": GradientBoostRegressor(),
+                "Gradient Boosting": GradientBoostRegressor(),
                 "Linear Regression": LinearRegression(),
                 "K-Nearest Neighbors": KNeighborsRegressor(),
-                # "XG-Boost Classsifier": XGBRegressor(),
-                # "CatBoosting Classifier": CatBoostRegressor(),
+                "XG-Boost Classsifier": XGBRegressor(),
+                "CatBoosting Classifier": CatBoostRegressor(),
                 "AdaBoost Classifier": AdaBoostRegressor(),
             }
 
@@ -70,25 +70,25 @@ class ModelTrainer:
                     # 'max_features':['sqrt','log2',None],
                     "n_estimators": [8, 16, 32, 64, 128, 256]
                 },
-                # "Gradient Boosting": {
-                #     # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                #     "learning_rate": [0.1, 0.01, 0.05, 0.001],
-                #     "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
-                #     # 'criterion':['squared_error', 'friedman_mse'],
-                #     # 'max_features':['auto','sqrt','log2'],
-                #     "n_estimators": [8, 16, 32, 64, 128, 256],
-                # },
+                "Gradient Boosting": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
                 "Linear Regression": {},
                 "K-Nearest Neighbors": {},
-                # "XGBRegressor": {
-                #     "learning_rate": [0.1, 0.01, 0.05, 0.001],
-                #     "n_estimators": [8, 16, 32, 64, 128, 256],
-                # },
-                # "CatBoosting Regressor": {
-                #     "depth": [6, 8, 10],
-                #     "learning_rate": [0.01, 0.05, 0.1],
-                #     "iterations": [30, 50, 100],
-                # },
+                "XGBRegressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "CatBoosting Regressor": {
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "iterations": [30, 50, 100],
+                },
                 "AdaBoost Classifier": {
                     "learning_rate": [0.1, 0.01, 0.5, 0.001],
                     # 'loss':['linear','square','exponential'],
